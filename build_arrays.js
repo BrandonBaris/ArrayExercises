378daf72db6436c854c1e9f3fb86dea30b8538eb
Brandon Baris <brandonbaris@gmail.com>
2015-01-20 17:07:37 -1000

    initial commit

--- build_arrays.js
+++ build_arrays.js
@@ -3,54 +3,54 @@
  **/
 
 // Create an empty array.
-
+var empty_array = [];
 
 // Create an array of the colors of the rainbow.
-
+var rainbow = ["red","orange","yellow","green","blue","indigo","violet"];
 
 // Create an array of gems.
-
+var gems = ["diamond","sapphire","ruby","carnelite","emerald","opal"];
 
 // Create an array of military ranks.
-
+var military_ranks = ["private","sergeant","corporal","lieutenant","general","commander"];
 
 // Create an array of road signs.
-
+var road_signs = ["stop","slow","no right turn","yield"];
 
 // Create an array containing components of a tree.
-
+var tree_parts = ["bark","roots","canopy","trunk"];
 
 // Create an array of prime numbers less than 20.
-
+var primenum = [1,2,3,5,7,11,13,17,19];
 
 // Create an array of words programmers typically say while programming.
-
+var progwords = ["shit","dammit","why is it not working","i dont get it","syntax error","debug","my god why","commit"];
 
 // Create an array of elements of the periodic table.
-
+var elements = ["iron","carbon","hydrogen","oxygen","uranium"];
 
 // Create an array representing the coins currently in your pocket.
-
+var coins = ["quarter","penny","dime","nickel"];
 
 // Create an array of items you bring to class daily.
-
+var dailyitems = ["laptop","coffee","phone","edc knife"];
 
 // Create an array of web developer role titles.
-
+var webtitles = ["designer","UI developer","script kiddy"];
 
 // Create an array of good luck charms.
-
+var luckcharm = ["coin","rabbit foot","4 leaf clover","hair","war trophy"];
 
 // Create an array of mythical animals.
-
+var mythanimals = ["phoenix","chimera","dragon","hydra","wyvern","basilisk","ogre"];
 
 // Create an array of drinks.
-
+var drinks = ["soda","milk","juice","beer","wine"];
 
 // Create an array of emoticons.
-
+var emoticons = ["happy","sad","dealwithit"];
 
 // Create an array with the letters of the word 'array'.
-
+var spellarray = ["a","r","r","a","y"];
 
 
